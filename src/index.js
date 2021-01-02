12f507cfcd8c91e166f6e3c665de1775b28e1966
AleksandrAnisenko <aleksandr.anisenko@mail.ru>
2021-01-02 21:19:28 +0300

    test commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import example from './images/example.png'
 import './styles/main.scss'
 import './scripts/logout.js'
 import './scripts/changeLogInSignIn.js'
 import './scripts/signInForm.js'
 import './scripts/logInForm.js'
 
-const next = document.getElementById('next');
-next.addEventListener('click', function(){
-    document.location.href = "index2.html";
-});
-const back = document.getElementById('back');
-back.addEventListener('click', function(){
-    document.location.href = "index.html";
-})
+    
+   
+    
+    const next = document.getElementById('next');
+    next.addEventListener('click', function(){
+        document.location.href = "index2.html";
+    });
+
+    if (location.href == 'index.html2'){
+    const back = document.getElementById('back');
+    back.addEventListener('click', function(){
+        document.location.href = "index.html";
+    })
+    }
+
